fd7b15a81219991b06dac06e57c58ba8ab4fd547
wanghaisheng <wanghaisheng@saicmobility.com>
2019-12-11 10:13:55 +0800

    (JIRA NXYW-1633) http://jira.saicmobility.com:8080/browse/NXYW-1633

---
+++ src/main/java/com/hs/go/JustDo.java
@@ -0,0 +1,12 @@
+package com.hs.go;
+
+/**
+ * @author wanghaisheng
+ * description: <
+ * date: 2019/12/11 10:12<
+ */
+public class JustDo {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
