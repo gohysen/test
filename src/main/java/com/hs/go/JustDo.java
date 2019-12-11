12f8a31ed33b1e8a750f924f9149f72d4de82f07
wanghaisheng <wanghaisheng@saicmobility.com>
2019-12-11 10:37:56 +0800

    (JIRA NXYW-1633) http://jira.saicmobility.com:8080/browse/NXYW-1633

--- src/main/java/com/hs/go/JustDo.java
+++ src/main/java/com/hs/go/JustDo.java
@@ -10,5 +10,6 @@ public class JustDo {
         System.out.println("master");
         System.out.println("dev");
         System.out.println("---------------");
+        System.out.println("second=================================");
     }
 }
